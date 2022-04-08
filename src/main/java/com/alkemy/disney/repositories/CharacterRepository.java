5864d9a9a144ea593efc26e011469a4069652d42
Juan Manuel <juanmanuel.manuel70@gmail.com>
2022-04-08 12:07:32 -0300

    filtros criteria builder funcionando en characters

--- src/main/java/com/alkemy/disney/repositories/CharacterRepository.java
+++ src/main/java/com/alkemy/disney/repositories/CharacterRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import com.alkemy.disney.entities.CharacterEntity;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacterRepository extends PagingAndSortingRepository<CharacterEntity, Long> {
+public interface CharacterRepository extends PagingAndSortingRepository<CharacterEntity, Long>, JpaSpecificationExecutor<CharacterEntity> {
 
     CharacterEntity findByCharacterId(String characterId);
     List<CharacterEntity> findByNameIgnoreCaseContaining(String name);
