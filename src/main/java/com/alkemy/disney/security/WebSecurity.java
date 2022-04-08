bb0491f15073b40dd0ae19fccca3665cdc0b2825
Juan Manuel <juanmanuel.manuel70@gmail.com>
2022-04-07 23:29:36 -0300

    ajuste en websecurity

--- src/main/java/com/alkemy/disney/security/WebSecurity.java
+++ src/main/java/com/alkemy/disney/security/WebSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -24,7 +25,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-        .authorizeRequests().antMatchers(HttpMethod.POST, "/auth/register").permitAll();
+        .authorizeRequests().antMatchers(HttpMethod.POST, "/auth/register").permitAll()
+        .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
